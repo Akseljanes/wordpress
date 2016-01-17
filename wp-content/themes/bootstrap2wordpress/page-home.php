0a4f478540c87a4a31eed3e55bf3c560f6c3feb6
Aksel Jänes <aksel.janes@khk.ee>
2016-01-18 01:18:12 +0200

    Lecture 47  [#110074366]

--- wp-content/themes/bootstrap2wordpress/page-home.php
+++ wp-content/themes/bootstrap2wordpress/page-home.php
@@ -3,6 +3,15 @@
     Template Name: Home Page
  */
 
+// Custom Fields
+$prelaunch_price        = get_post_meta(7, 'prelaunch_price', true);
+$launch_price           = get_post_meta(7, 'launch_price', true);
+$final_price            = get_post_meta(7, 'final_price', true);
+$course_url             = get_post_meta(7, 'course_url', true);
+$button_text            = get_post_meta(7, 'button_text', true);
+$optin_text             = get_post_meta(7, 'optin_text', true);
+$optin_button_text      = get_post_meta(7, 'optin_button_text', true);
+
 get_header(); ?>
 
 <!-- HERO -->
@@ -16,26 +25,26 @@ get_header(); ?>
                 </div>
 
                 <div class="col-sm-7 hero-text">
-                    <h1>Bootstrap to WordPress</h1>
-                    <p class="lead">Earn An Extra $1k - $5k a Month by Learning to Code Your Very Own Responsive &amp; Custom WordPress Websites with Bootstrap</p>
+                    <h1><?php bloginfo('name'); ?></h1>
+                    <p class="lead"><?php bloginfo('description') ?></p>
                     <div id="price-timeline">
                         <div class="price active">
                             <h4>Pre-Launch Price <small>Ends soon</small></h4>
-                            <span>$149</span>
+                            <span><?php echo $prelaunch_price; ?></span>
                         </div>
 
                         <div class="price">
                             <h4>Launch Price <small>Coming soon</small></h4>
-                            <span>$299</span>
+                            <span><?php echo $launch_price; ?></span>
                         </div>
 
                         <div class="price">
                             <h4>Final Price <small>Coming soon</small></h4>
-                            <span>$399</span>
+                            <span><?php echo $final_price; ?></span>
                         </div>
                     </div>
 
-                    <p><a class="btn btn-lg btn-danger" href="/" role="button">Enroll now &raquo;</a></p>
+                    <p><a class="btn btn-lg btn-danger" href="<?php echo $course_url; ?>" role="button"><?php echo $button_text; ?></a></p>
 
                 </div>
 
@@ -50,11 +59,11 @@ get_header(); ?>
     <div class="container">
         <div class="row">
             <div class="col-sm-8">
-                <p class="lead"><strong>Subscribe to our mailing list</strong> We will send something special as a thank you</p>
+                <p class="lead"><?php echo $optin_text; ?></p>
             </div>
             <div class="col-sm-4">
                 <button class="btn btn-success btn-lg btn-block" data-toggle="modal" data-target="#myModal">
-                    Click here to subscribe!
+                    <?php echo $optin_button_text; ?>
                 </button>
             </div>
         </div>
