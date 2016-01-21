7c82eb5da4232338239551c0c8b81eb5ba5c895a
Aksel Jänes <aksel.janes@khk.ee>
2016-01-21 22:12:36 +0200

    Lecture 58 [#110074516]

---
+++ wp-content/themes/bootstrap2wordpress/page-resources.php
@@ -0,0 +1,53 @@
+<?php
+/*
+ Template Name: Resources page
+*/
+
+?>
+
+<!-- FEATURE IMAGE -->
+<section class="feature-image feature-image-default" data-type="background" data-speed="2">
+    <h1 class="page-title">Resources</h1>
+</section>
+
+<!-- MAIN CONTENT -->
+
+<div class="container">
+    <div class="row" id="primary">
+        <div id="content" class="col-sm-12">
+            <section class="main-content">
+                <p class="lead">Welcome to the Bootstrap resources Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis consectetur deleniti ipsam magnam! Consectetur eius eligendi, eos eveniet minus nihil pariatur qui rem reprehenderit totam. Asperiores deleniti ipsam iure repellendus!</p>
+                <p><em>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis commodi ea est ipsam laborum non officia repellat sequi voluptates voluptatibus! Aliquid asperiores esse facere in nesciunt odit perferendis similique suscipit.</em></p>
+
+                <hr>
+
+                <div class="resource-row clearfix">
+                    <div class="resource">
+                        <img src="assets/img/justhost-logo.jpg" alt="JustHost">
+                        <h3><a href="google.com">JustHost</a></h3>
+                        <p>When I made my very first website about 7 years ago, I signed up with Justhost — and I’ve never looked back! Since then, I’ve built hundreds of websites with Justhost as my only hosting provider. I recommend these guys to all of my clients, my students, my family and friends.</p>
+                        <p>If you’re looking to start your very first website, or are looking for a reliable hosting provider — look no further. You get a free domain name for life, great customer service, unlimited domains, unlimited email accounts, unlimited GBs of space, an anytime money back guarantee and my favourite part: You can set up a blog website in minutes with 1-click WordPress Install. These guys are awesome!</p>
+                        <a href="google.com" class="btn btn-success">Get Started With JustHost!</a>
+                    </div>
+
+                    <div class="resource">
+                        <img src="assets/img/coda2-logo.jpg" alt="Coda 2">
+                        <h3><a href="google.com">JustHost</a></h3>
+                        <p>If you’re serious about coding, or you want to start out with a solid tool to code with, I strongly recommend Coda 2 for Mac. A beautiful User Interface with tabbed navigation, a super sidebar, and a simple layout. The features are outrageous and extensive, with code folding, smart complete, auto indentation, GIT integration, a built-in terminal, iCloud support — to name a few. The features seem to be endless.</p>
+                        <p>I use Coda 2 for all of my courses & tutorials, and if you decide to use Coda 2 as your editor of choice, it would greatly benefit you when taking my courses. The price of the editor weighs in at $75, but it’s worth every penny — in my honest opinion.</p>
+                        <a href="google.com" class="btn btn-success">Get Started With Coda 2!</a>
+                    </div>
+
+                    <div class="resource">
+                        <img src="assets/img/dropbox-logo.jpg" alt="DropBox">
+                        <h3><a href="google.com">JustHost</a></h3>
+                        <p>This tool should go without saying. But if I have to explain — you need this tool. I use it 24/7 — literally. Dropbox is where I store my personal & business related data. It’s accessible anywhere, and regularly backs up my important information.</p>
+                        <p>Because my data is accessible anywhere, Dropbox allows me to work on a client project at home, on the bus, in a coffee shop, or at a friend’s house. It’s truly a gem of a tool, and I think you’ll believe me.</p>
+                        <a href="google.com" class="btn btn-success">Get Started With DropBox!</a>
+                    </div>
+                </div>
+
+            </section>
+        </div>
+    </div>
+</div>
